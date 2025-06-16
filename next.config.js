7264c5899aa8d13a092b241df18edb188c90f118
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-06-16 21:06:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+const { i18n } = require('./next-i18next.config.js');
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development'
-})
+});
 
 module.exports = withPWA({
-  reactStrictMode: true
-})
+  reactStrictMode: true,
+  i18n,
+});
