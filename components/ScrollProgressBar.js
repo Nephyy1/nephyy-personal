01ff6a10898e32be3beb9d38acc8a6a41c5fddc0
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-06-16 19:29:46 +0700

    Create ScrollProgressBar.js

---
+++ components/ScrollProgressBar.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+export default function ScrollProgressBar() {
+  const [scroll, setScroll] = useState(0);
+
+  const onScroll = () => {
+    const Scrolled = document.documentElement.scrollTop;
+    const MaxHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const ScrollPercent = (Scrolled / MaxHeight) * 100;
+    setScroll(ScrollPercent);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  return (
+    <div className="fixed top-0 left-0 w-full h-1 bg-gray-200 z-50">
+      <div
+        className="h-1 bg-blue-500"
+        style={{ width: `${scroll}%` }}
+      ></div>
+    </div>
+  );
+}
