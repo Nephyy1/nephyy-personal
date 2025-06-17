7175cbd52b9afc25851b43c60627705a818449c8
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-06-17 14:24:40 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -186,33 +186,35 @@ export default function Home() {
           </div>
 
           <div
-              className="absolute w-full h-1/3 md:h-2/5 -bottom-1/4 left-0 z-10"
-              style={{ transform: `translateY(${offsetY * 0.2}px)` }}
-          >
-              <Image src="/white cloud.webp" layout="fill" objectFit="cover" alt="cloud layer" />
-          </div>
-          <div
-              className="absolute w-32 h-32 bottom-5 left-5 md:w-48 md:h-48 md:bottom-16 md:left-10 z-10"
+              className="absolute w-32 h-32 bottom-2 left-2 md:w-48 md:h-48 md:bottom-16 md:left-10 z-10"
               style={{ transform: `translateY(${offsetY * 0.45}px)` }}
           >
               <Image src="/cloud 1.webp" layout="fill" objectFit="contain" alt="cloud 1" />
           </div>
           <div
-              className="absolute w-40 h-40 bottom-2 -right-5 md:w-64 md:h-64 md:bottom-10 md:right-10 z-10"
+              className="absolute w-36 h-36 bottom-2 right-2 md:w-64 md:h-64 md:bottom-10 md:right-10 z-10"
               style={{ transform: `translateY(${offsetY * 0.3}px)` }}
           >
               <Image src="/cloud 2.webp" layout="fill" objectFit="contain" alt="cloud 2" />
           </div>
 
           <div
-              className="absolute w-32 h-48 md:w-48 md:h-72 top-1/2 left-1/2 z-20"
+              className="absolute w-32 h-48 md:w-48 md:h-72 top-[68%] md:top-[65%] left-1/2 z-10"
               style={{
-                transform: `translate(-50%, calc(-50% + ${offsetY * 0.1}px)) rotate(${offsetY * 0.05}deg)`,
+                transform: `translate(-50%, -50%) rotate(${offsetY * 0.05}deg)`,
                 opacity: Math.max(0, 1 - offsetY / 500),
               }}
           >
               <Image src="/new astro.webp" layout="fill" objectFit="contain" alt="Astronaut" />
           </div>
+          
+          <div
+              className="absolute w-full h-1/3 md:h-2/5 -bottom-1/4 left-0 z-20"
+              style={{ transform: `translateY(${offsetY * 0.2}px)` }}
+          >
+              <Image src="/white cloud.webp" layout="fill" objectFit="cover" alt="cloud layer" />
+          </div>
+
         </section>
 
         <section id="about" className="py-20 bg-white relative overflow-hidden z-20">
