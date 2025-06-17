52ce53de43d241cdb5c33296742f7442cd272cef
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-06-17 09:01:21 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import { appWithTranslation } from 'next-i18next';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ParallaxProvider>
+      <Component {...pageProps} />
+    </ParallaxProvider>
+  );
 };
 
 export default appWithTranslation(MyApp);
